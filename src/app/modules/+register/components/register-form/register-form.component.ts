e01138c2a0283a1872edbeddfa07556621ab28f0
Esteban García Pérez <egarcia@profile.es>
2024-05-08 13:21:21 +0200

    Migrated FormGroup to FormBuilder, other design changes

--- src/app/modules/+register/components/register-form/register-form.component.ts
+++ src/app/modules/+register/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from '../../../../shared/services/user.service';
@@ -7,33 +7,40 @@ import { User } from '../../../../shared/models/user/user.model';
 import { NgIf } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, NgIf,],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    NgIf,
+    MatCardModule
+  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
-  constructor(private userService: UserService, private snackBar: MatSnackBar) { }
+  constructor(private userService: UserService, private snackBar: MatSnackBar, private fb: FormBuilder) { }
 
-  loginForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')])
+  registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    surname: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    phone: ['', [Validators.required, Validators.pattern(/^[0-9]+$/)]]
   });
 
   onSubmit() {
-    if (this.loginForm.valid) {
+    if (this.registerForm.valid) {
       const formData: User = {
-        name: this.loginForm.value.name || "",
-        surname: this.loginForm.value.surname || "",
-        email: this.loginForm.value.email || "",
-        password: this.loginForm.value.password || "",
-        phone: this.loginForm.value.phone || ""
+        name: this.registerForm.value.name || "",
+        surname: this.registerForm.value.surname || "",
+        email: this.registerForm.value.email || "",
+        password: this.registerForm.value.password || "",
+        phone: this.registerForm.value.phone || ""
       };
 
       this.userService.addUser(formData).subscribe({
