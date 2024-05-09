91a3ffa83fe8fbad509b346c31723b59a97aad12
Esteban García Pérez <egarcia@profile.es>
2024-05-09 11:18:46 +0200

    Migrated variable reference

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,20 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user/user.model';
 import { Observable, catchError, of, tap, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private userUrl = 'http://localhost:8080/api/v1/user'
-
   constructor(private http: HttpClient) { }
 
   addUser(user: User): Observable<User> {
-    return this.http.post<User>(this.userUrl, user).pipe(
+    return this.http.post<User>(environment.userUrl, user).pipe(
       catchError((error: HttpErrorResponse) => {
         console.error(`addUser failed: ${error.message}`);
-        return throwError(() => error); // Rethrow the actual error object
+        return throwError(() => error);
       })
     );
   }
